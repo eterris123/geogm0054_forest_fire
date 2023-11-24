bec7619a302a4dbc486ee35e3c3c02bb421f960e
Ethan Terris <ethanterris@hotmail.com>
2023-11-24 10:00:41 +0000

    new growth experiment

--- ca_fire_growth_experiment.py
+++ ca_fire_growth_experiment.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Oct  4 20:35:51 2022
+Created on Fri Nov  24 09:58:51 2023
 
-@author: steph
+@author:Ethan
 """
 from ca_fire import evolve_forest, write_netcdf
 
